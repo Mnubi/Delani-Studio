24802cc4d1b1f07ef5eb1ccb9203cd03a1f51414
mnubi <rodger.kamau@student.moringaschool.com>
2021-09-16 21:07:12 +0300

    added bounce effect and hide the contents what we do

--- js/main.js
+++ js/main.js
@@ -1,3 +1,64 @@
-submit = function () {
+$(document).ready(function (){
+    
+    
+    //show and hide paragraph on click
 
-};
+    $("#design").on('click',function () {
+        $("#design").hide();
+        $(designpg).show(); 
+      
+    });
+
+    $("#design").on('click',function () {
+        $("#design").hide();
+        $(designpg).show(); 
+      
+    });
+
+    
+    $('#dev').on('click',function  () {
+        $("#devpg").show();
+        $('#dev').hide();
+        
+    });
+
+    $('#design').on('click',function  () {
+        $("#dev").show();
+        $('#product').show();
+        
+    });
+
+    $('#product').on('click',function  () {
+        $("#productpg").show();
+        $('#product').hide();
+        
+    });
+
+
+
+
+
+
+
+
+
+// ///
+// $(document).ready(function () {
+//     $('#form').on('submit', function () {
+//         e.preventDefault();
+
+//         let name=$("#name");
+//         let email=$("#email");
+//         let message=$("#message");
+
+//         ) {
+//      if (name="" email="" message="") {
+//                 alert('We have received your message.Our team will get back to you as soon as possible. Thank you for reaching out to us');
+//                 $('form.contact');
+//             } else {
+//                 alert("All fields are required");
+//             }
+//         };
+//     };
+
+});
